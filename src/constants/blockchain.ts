a3b99e30b70c3f2f03d7150fdd64b576d40e7eb2
Phuong Nguyen <phuong.nguyennam@digipaysolution.com>
2022-06-16 09:42:16 +0700

    wating pancake address

--- src/constants/blockchain.ts
+++ src/constants/blockchain.ts
@@ -3,8 +3,8 @@ export const TOKEN_DECIMALS = 9;
 export enum Networks {
     AVAX = 97,
     BSC_MAINNET = 56,
+    HARMONY_TESTNET = 1666700000,
+    HARMONY = 1666600000,
 }
 
-console.log("hshaha", process.env.NODE_ENV);
-
-export const DEFAULT_NETWORK = process.env.NODE_ENV === "production" ? Networks.BSC_MAINNET : Networks.AVAX;
+export const DEFAULT_NETWORK = process.env.NODE_ENV === "production" ? Networks.HARMONY : Networks.HARMONY_TESTNET;
