d5e283dd7028fc466bfe02b8e65a707b9ce3c4f6
Tam Nguyen <nntam2013@gmail.com>
2022-06-21 22:56:35 +0700

    feat: deploy testnet

--- migrations/2_deploy_busd_bond_depository.js
+++ migrations/2_deploy_busd_bond_depository.js
@@ -4,15 +4,13 @@ const TimeBondDepository = artifacts.require("TimeBondDepository");
 module.exports = async function (deployer, network) {
     let token = "";
 
-    let pancakeAggregator = ""; // Pancake Router in BSC, Sushi Router in Harmony
+    let pancakeRouter = ""; // Pancake Router in BSC, Sushi Router in Harmony
     let principle = ""; // WBNB in BSC, WONE in Harmony
-    let pancakeRouter = "";
     let treasuryAddress = "";
     let DAOAddress = "";
     let usd = ""; // BUSD in BSC, USDC in Harmony
     if (network === "bsctestnet") {
         token = "0x28ad774C41c229D48a441B280cBf7b5c5F1FED2B"; // xBlade (receive token)
-        pancakeAggregator = "0x89db444c9542f8ea79b944be460b3efa077f5962";
         principle = "0xae13d989dac2f0debff460ac112a837c89baa7cd";
         pancakeRouter = "0x9ac64cc6e4415144c455bd8e4837fea55603e5c3";
         treasuryAddress = "0x14CF6cbBede5107fDD1D99cE9eC075Cb1Fa273c8";
@@ -22,7 +20,6 @@ module.exports = async function (deployer, network) {
 
     if (network === "bscmainnet") {
         token = "0x27a339d9b59b21390d7209b78a839868e319301b"; // xBlade (receive token)
-        pancakeAggregator = "0x33890Bf7d99b4afeD80F651Cb7dd1b12EB26597D";
         principle = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
         pancakeRouter = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
         treasuryAddress = "0xa60fb437b969c7e2d8c5927b9dcfe9a3cb597b5b";
