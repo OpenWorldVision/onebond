03fa26c5cd5b0052495baa381690b18e3486182e
Tam Nguyen <nntam2013@gmail.com>
2022-08-05 16:25:30 +0700

    fix: market price from coingecko

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -2,11 +2,17 @@ import { ethers } from "ethers";
 import { LpReserveContract } from "../abi";
 import { xbladeBUSD } from "../helpers/bond";
 import { Networks } from "../constants/blockchain";
+import axios from "axios";
 
 export async function getMarketPrice(networkID: Networks, provider: ethers.Signer | ethers.providers.Provider): Promise<number> {
-    const mimTimeAddress = xbladeBUSD.getAddressForReserve(networkID);
-    const pairContract = new ethers.Contract(mimTimeAddress, LpReserveContract, provider);
-    const reserves = await pairContract.getReserves();
-    const marketPrice = reserves[0] / reserves[1];
-    return marketPrice;
+    const result = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=cryptowar-xblade&vs_currencies=usd");
+    if (result.data["cryptowar-xblade"]?.usd) {
+        return 1 / result.data["cryptowar-xblade"]?.usd;
+    }
+    return 0;
+    // const mimTimeAddress = xbladeBUSD.getAddressForReserve(networkID);
+    // const pairContract = new ethers.Contract(mimTimeAddress, LpReserveContract, provider);
+    // const reserves = await pairContract.getReserves();
+    // const marketPrice = reserves[0] / reserves[1];
+    // return marketPrice;
 }
